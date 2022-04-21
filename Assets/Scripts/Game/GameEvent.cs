4d9e0eca0b562bf9274bf4146b9584074dc9a265
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2022-04-21 00:20:05 -0500

    Created an abstract event system woo
    Also gonna kick a cat later today

---
+++ Assets/Scripts/Game/GameEvent.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Game
+{
+    public class GameEvent
+    {
+        enum EventTy
+        { 
+            OnCardMove, // Card Moved Locations - can use for unity animations. Has to pass in an object that knows the two locations that it moved from
+            OnCardResolve, // Card is resolved from the stack.
+            OnPlayerDraw, // a player draws a card
+            OnDamageRecieved, // a player takes damage from a source
+            SIZE,   // Number of events
+        }
+
+        // queueing order for an event can possibly be important
+        // data could mean literally whatever you want it to mean
+        public delegate void EventObserver(GameInstance state, object data);
+
+        List<EventObserver> _mapping;
+
+        void Attach(EventTy type, EventObserver observer)
+        {
+            _mapping[(int)type] += observer;
+        }
+        void Detach(EventTy type, EventObserver observer)
+        {
+            _mapping[(int)type] += observer;
+        }
+        void Notify(EventTy type, GameInstance state, object data)
+        {
+            _mapping[(int)type].Invoke(state, data);
+        }
+
+        public GameEvent()
+        {
+            _mapping = new List<EventObserver>((int)EventTy.SIZE);
+
+            for(int i = 0; i < _mapping.Count; i++)
+            {
+                _mapping[i] = delegate { };
+            }
+        }
+    }
+}
